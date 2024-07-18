1ae520cf932ec949e354d0dc5f38566f08ab5354
Ame314 <133021649+Ame314@users.noreply.github.com>
2024-07-18 16:29:51 -0500

    :(
    -hacer que funcione el menú de navegación interna.
    -Escribir descripciones adecuadas para cada proyecto además de encontrar una imagen o crear una con IA.
    -colocar correctamente los links a los repositorios y ciertamente arreglar el proyecto.
    -crear el proyecto de 2048 y el de Jaime.
    -arreglar los contenedores de los proyectos.
    -refactorizar el código
    -ver si se puede agregar efectos y cómo hacerlo.

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import {
   Text,
   View,
-  StyleSheet,
   TouchableOpacity,
   Linking,
   Image,
@@ -10,10 +9,11 @@ import {
   Modal,
   Animated,
 } from "react-native";
+import {styles} from '../carpetita/estilos'; // Importando los estilos 
 
 export default function Home() {
   const [modalVisible, setModalVisible] = useState(false);
-  const [modalContent, setModalContent] = useState("");
+  const [modalContent, setModalContent] = useState("11");
   const [fadeInAnim] = useState(new Animated.Value(0)); // Animación para el fade-in
 
   useEffect(() => {
@@ -30,10 +30,10 @@ export default function Home() {
   };
 
   const projects = [
-    { title: "Juego 4 en línea", description: "Este es un juego desarrollado en Java.", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAtSlf1LjXZDCFYCcv5KdJvkLKNPU-Et1XIw&s", link: "https://github.com/Ame314/juego4enlinea" },
-    { title: "Ahorcado", description: "Juego del ahorcado desarrollado en c#.", image: "https://i.pinimg.com/736x/94/5d/81/945d81b5b521e4e9ee60b78c511043ef.jpg", link: "https://github.com/Ame314/ahorcado" },
-    { title: "3 en línea", description: "Juego 3 en línea desarrollado en c#.", image: "https://papergames.io/es/assets/games/tictactoe/thumbnail.png", link: "https://github.com/Ame314/3enlinea" },
-    { title: "2048", description: "Juego 2048 desarrollado en Python.", image: "https://fscl01.fonpit.de/userfiles/4774964/image/AndroidPIT-2048-win.jpg", link: "https://github.com/Ame314/2048" },
+    { title: "Connect 4", description: "This is a game developed in Java.", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAtSlf1LjXZDCFYCcv5KdJvkLKNPU-Et1XIw&s", link: "https://github.com/Ame314/juego4enlinea" },
+    { title: "Hangman", description: "Juego del ahorcado desarrollado en c#.", image: "https://i.pinimg.com/736x/94/5d/81/945d81b5b521e4e9ee60b78c511043ef.jpg", link: "https://github.com/Ame314/ahorcado" },
+    { title: "Noughts and crosses ", description: "noughts and crosses game developed in c#.", image: "https://papergames.io/es/assets/games/tictactoe/thumbnail.png", link: "https://github.com/Ame314/3enlinea" },
+    { title: "2048", description: "2048 game developed in Python.", image: "https://fscl01.fonpit.de/userfiles/4774964/image/AndroidPIT-2048-win.jpg", link: "https://github.com/Ame314/2048" },
   ];
 
   const renderProjects = () =>
@@ -54,29 +54,35 @@ export default function Home() {
         <Text style={styles.logoText}>AG</Text>
         <View style={styles.navLinks}>
           <TouchableOpacity onPress={() => { }}>
-            <Text style={styles.navLink}>Inicio</Text>
+            <Text style={styles.navLink}>Home</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => { }}>
-            <Text style={styles.navLink}>Proyectos</Text>
+            <Text style={styles.navLink}>About me</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => { }}>
-            <Text style={styles.navLink}>Certificados</Text>
+            <Text style={styles.navLink}>Skills</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => { }}>
-            <Text style={styles.navLink}>Sobre mí</Text>
+            <Text style={styles.navLink}>Projects</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { }}>
+            <Text style={styles.navLink}>Certificates</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { }}>
+            <Text style={styles.navLink}>Contact me</Text>
           </TouchableOpacity>
         </View>
       </View>
       <View style={styles.heroSection}>
         <Text style={styles.heroTitle}>
-          Soy <Text style={styles.heroName}>Amelie Grob</Text>, Junior Developer
+          I´m <Text style={styles.heroName}>Amelie Grob</Text>, Junior Developer
         </Text>
-        <Text style={styles.heroSubtitle}>Tercer nivel en tecnologías de la Información</Text>
+        <Text style={styles.heroSubtitle}>Third level in Information Technology.</Text>
       </View>
 
       <View style={styles.profileSection}>
         <Text style={styles.description}>
-          Cada día aprendiendo algo nuevo.
+        Every day I learn something new.
         </Text>
       </View>
 
@@ -156,10 +162,9 @@ export default function Home() {
           style={styles.certificateBox}
           onPress={() => openModal("Description of the certificate.")}
         >
-          {/* Replace with your certificate image */}
           <Image
             source={{
-              uri: "https://example.com/certificate-image.jpg",
+              uri: "https://web.whatsapp.com/3c2def66-5620-4645-99bd-5adacc5a9d6c",
             }}
             style={styles.certificateImage}
           />
@@ -233,245 +238,5 @@ export default function Home() {
         <Text style={styles.footerText}>© 2024 Ame Grob</Text>
       </View>
     </ScrollView>
-  );
-}
+);}
 
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    backgroundColor: "#0d0d0d",
-    alignItems: "center",
-    paddingVertical: 20,
-  },
-  header: {
-    width: "100%",
-    backgroundColor: "#000000",
-    padding: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  logoText: {
-    fontSize: 24,
-    color: "#800080", // Morado
-    fontWeight: "bold",
-  },
-  navLinks: {
-    flexDirection: "row",
-  },
-  navLink: {
-    fontSize: 18,
-    color: "#ffffff",
-    marginHorizontal: 10,
-  },
-  heroSection: {
-    width: "100%",
-    padding: 40,
-    backgroundColor: "#000000",
-    alignItems: "center",
-  },
-  heroTitle: {
-    fontSize: 36,
-    color: "#ffffff",
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 10,
-  },
-  heroName: {
-    color: "#800080", // Morado
-  },
-  heroSubtitle: {
-    fontSize: 18,
-    color: "#ffffff",
-    textAlign: "center",
-  },
-  profileSection: {
-    width: "100%",
-    backgroundColor: "#0d0d0d",
-    paddingHorizontal: 30,
-    paddingVertical: 15,
-    alignItems: "center",
-  },
-  description: {
-    fontSize: 18,
-    color: "#ffffff",
-    textAlign: "center",
-  },
-  blackBar: {
-    width: "90%",
-    height: 2,
-    backgroundColor: "#800080", // Morado
-    marginVertical: 20,
-  },
-  aboutSkillsSection: {
-    width: "100%",
-    paddingHorizontal: 30,
-    paddingVertical: 15,
-    backgroundColor: "#0d0d0d",
-  },
-  aboutSection: {
-    width: "100%",
-    paddingRight: 20,
-    marginBottom: 20,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    color: "#ffffff",
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  aboutDescription: {
-    fontSize: 16,
-    color: "#ffffff",
-  },
-  skillsSeparator: {
-    width: "100%",
-    height: 2,
-    backgroundColor: "#800080", // Morado
-    marginVertical: 20,
-  },
-  skillsSection: {
-    width: "100%",
-    alignItems: "center",
-    backgroundColor: "#1a1a1a",
-    paddingVertical: 15,
-    marginBottom: 20,
-    borderRadius: 10,
-  },
-  skillsContainer: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-  },
-  skillsImage: {
-    width: 40,
-    height: 40,
-    marginHorizontal: 10,
-    marginBottom: 10,
-  },
-  purpleSeparator: {
-    width: "100%",
-    height: 2,
-    backgroundColor: "#800080", // Morado
-    marginVertical: 20,
-  },
-  projectsSection: {
-    width: "100%",
-    paddingHorizontal: 30,
-    paddingVertical: 15,
-    backgroundColor: "#0d0d0d",
-  },
-  projectsRow: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-  },
-  projectBox: {
-    width: "48%",
-    backgroundColor: "#1a1a1a",
-    borderRadius: 10,
-    padding: 10,
-    marginBottom: 20,
-    alignItems: "center",
-  },
-  projectImage: {
-    width: "100%",
-    height: 100,
-    borderRadius: 10,
-  },
-  projectTitle: {
-    fontSize: 16,
-    color: "#ffffff",
-    fontWeight: "bold",
-    marginTop: 10,
-    textAlign: "center",
-  },
-  certificatesSection: {
-    width: "100%",
-    paddingHorizontal: 30,
-    paddingVertical: 15,
-    backgroundColor: "#0d0d0d",
-  },
-  certificateBox: {
-    width: "48%",
-    backgroundColor: "#1a1a1a",
-    borderRadius: 10,
-    padding: 10,
-    marginBottom: 20,
-    alignItems: "center",
-  },
-  certificateImage: {
-    width: "100%",
-    height: 200,
-    borderRadius: 10,
-  },
-  modalBackground: {
-    flex: 1,
-    backgroundColor: "rgba(0, 0, 0, 0.8)",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  modalContainer: {
-    width: "80%",
-    backgroundColor: "#1a1a1a",
-    borderRadius: 10,
-    padding: 20,
-    alignItems: "center",
-  },
-  modalText: {
-    fontSize: 16,
-    color: "#ffffff",
-    textAlign: "center",
-    marginBottom: 20,
-  },
-  closeButton: {
-    marginTop: 10,
-    padding: 10,
-    backgroundColor: "#800080", // Morado
-    borderRadius: 5,
-  },
-  closeButtonText: {
-    fontSize: 16,
-    color: "#ffffff",
-    textAlign: "center",
-  },
-  footer: {
-    width: "100%",
-    backgroundColor: "#000000",
-    paddingVertical: 20,
-    alignItems: "center",
-  },
-  linksContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginBottom: 10,
-  },
-  projectImgcont: {
-    width: 40,
-    height: 40,
-    marginHorizontal: 10,
-  },
-  footerText: {
-    fontSize: 14,
-    color: "#ffffff",
-  },
-  aboutSection: {
-    width: "50%", // Ajusta el ancho según tu diseño
-    paddingRight: 20,
-  },
-  aboutHeader: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 10,
-  },
-  aboutImage: {
-    width: 60, // Ajusta el tamaño de la imagen según sea necesario
-    height: 60,
-    borderRadius: 30, // Hace que la imagen sea circular (ajusta según necesites)
-    marginRight: 10,
-  },
-  aboutDescription: {
-    fontSize: 16,
-    color: "#ffffff",
-  },
-});
